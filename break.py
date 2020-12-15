1a15298d638fe6fb03e2fbba7b347abd062912ad
woonayoung <dn2255520@gmail.com>
2020-12-15 13:32:20 +0900

    first

--- break.py
+++ break.py
@@ -1,4 +1,3 @@
-
 for row in range(10):
     if row == 2:
         break
@@ -19,21 +18,3 @@ for i in range(12):
 # 아래와 비슷
 for i in range(1, 10, 2):
     print(i)
-
-
-# while
-
-age = 0
-while age < 5:
-    print('{}살입니다.'.format(age))
-    age += 1
-
-
-age = 0
-num = 0
-while age < 5:
-    while num < 10:
-        print('{}살입니다.'.format(age))  # 10번 반복
-        num += 1
-    num = 0
-    age += 1
